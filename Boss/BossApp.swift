b9a87f2c7ce79b8baed8112ff5f1a4c750416bc2
EvanZhao <evanzhaoi@163.com>
2025-02-18 22:29:16 +0800

    Initial Commit

---
+++ Boss/BossApp.swift
@@ -0,0 +1,17 @@
+//
+//  BossApp.swift
+//  Boss
+//
+//  Created by 赵良皓 on 2025/2/18.
+//
+
+import SwiftUI
+
+@main
+struct BossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
